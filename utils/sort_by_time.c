f520757d9464e25f956c1fe1bc7bf76501f95044
Anton Medvedev <amedvede@e1z2r3p48.42.us.org>
2018-10-21 23:03:25 -0700

    final norm

--- utils/sort_by_time.c
+++ utils/sort_by_time.c
@@ -25,18 +25,15 @@ t_list		*sort_by_time(t_list *paths, int flag_reverse)
 		j = paths;
 		while (j->next)
 		{
-			lstat((char *)j->data, &st1);
-			lstat((char *)j->next->data, &st2);
+			lstat((char *)j->data, &st1) && lstat((char *)j->next->data, &st2);
 			if (flag_reverse ? st1.st_mtime > st2.st_mtime
 			: st1.st_mtime < st2.st_mtime)
 				node_swap(j, j->next);
 			else if (st1.st_mtime == st2.st_mtime)
-			{
-				if (flag_reverse ? st1.st_mtimespec.tv_nsec
-				> st2.st_mtimespec.tv_nsec
+				if (flag_reverse ? st1.st_mtimespec.tv_nsec >
+				st2.st_mtimespec.tv_nsec
 				: st1.st_mtimespec.tv_nsec < st2.st_mtimespec.tv_nsec)
 					node_swap(j, j->next);
-			}
 			j = j->next;
 		}
 		i = i->next;
